ae232798805c64855e1e2c4fbe8aecf73d7f78ef
Josh Bartow <jbartow92@gmail.com>
2023-08-10 11:01:35 -0500

    minor changes to typlist.js

--- scripts/TypeList.js
+++ scripts/TypeList.js
@@ -1,30 +1,35 @@
 import { getArchetypes } from "./database.js";
 
+const allArchetypes = getArchetypes();
 document.addEventListener("click", (clickEvent) => {
   const typeClicked = clickEvent.target;
-
+for (const archetype of allArchetypes) {
+  
   if (typeClicked.dataset.type === "type") {
     //What does type equal?
     const detailsHtml = `
     <div>
       <h4>Greeting:</h4>
-      <p>${greeting}</p>
+      <p>${archetype.greeting}</p>
       <h4>School:</h4>
-      <p>${school}</p>
+      <p>${archetype.schoolId}</p>
       <h4>Study Book:</h4>
-      <p>${study_book}</p>
+      <p>${archetype.bookId}</p>
     </div>`;
-    return detailsHtml
-  }
+  
+  
+}
+const parentTag = document.querySelector("#message");
+    parentTag.innerHTML = detailsHtml;
+}
 });
 
 export const TypeList = () => {
-  const allArchetypes = getArchetypes();
   let headerHtml = `<div id="archetypes_container" class="content_block">`;
   headerHtml += `<h3>Magician Types:</h3>`;
   let archetypeHTML = "<ul>";
   for (const archetype of allArchetypes) {
-    archetypeHTML += `<li>${archetype.name}</li>`;
+    archetypeHTML += `<li data-type="type" data-greeting="${archetype.greeting}" data-school="${archetype.schoolId}" data-study_book="${archetype.bookId}">${archetype.name}</li>`;
   }
   archetypeHTML += "</ul>";
   headerHtml += archetypeHTML;
